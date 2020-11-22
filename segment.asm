45a68e9a507e12aae8ab202cad42d0433afa034a
yutarofire <yutaro.ono.418@gmail.com>
2020-11-22 16:25:48 +0900

    Add comments for description

--- segment.asm
+++ segment.asm
@@ -2,20 +2,28 @@
 ; A simple boot sector program that demonstrates segment offsetting.
 ;
 
+; Segment offsetting calculation:
+;   <value in segment register> * 16 + <offset value>
+
 ; tty mode
 mov ah, 0x0e
 
+; Without offsetting, wrong address.
 mov al, [the_secret]
 int 0x10
 
+; Set the data segment register (ds).
+; (Note that it's not possible to set ds directly.)
 mov bx, 0x7c0
 mov ds, bx
 mov al, [the_secret]
 int 0x10
 
+; With extra (user defined) register.
 mov al, [es: the_secret]
 int 0x10
 
+; With extra (user defined) register, that set the value 0x7c0.
 mov bx, 0x7c0
 mov es, bx
 mov al, [es: the_secret]
